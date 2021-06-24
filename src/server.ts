0ba8f7d3697907047a3754cbbc7a23aa6d223649
Renato Castro <renatocastro02@hotmail.com>
2021-06-24 15:15:12 -0300

    chore: add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
-import express from "express";
+import express from 'express';
+import 'reflect-metadata';
+
+import './database';
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running"));
+app.use(express.json());
+
+app.listen(3000, () => console.log('Server is running'));
