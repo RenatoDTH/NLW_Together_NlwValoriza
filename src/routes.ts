f72c8cc2b8bda32ff4f27dc7b61ad32a90bee09b
Renato Castro <renatocastro02@hotmail.com>
2021-06-26 14:04:28 -0300

    feat: add ListUserReceiveCompliments

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
@@ -12,6 +13,8 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserReceiveComplimentsController =
+  new ListUserReceiveComplimentsController();
 
 router.post('/users', createUserController.handle);
 router.post(
@@ -25,6 +28,11 @@ router.post(
   '/compliments',
   ensureAuthenticated,
   createComplimentController.handle
+);
+router.get(
+  '/compliments/receive',
+  ensureAuthenticated,
+  listUserReceiveComplimentsController.handle
 );
 
 export { router };
